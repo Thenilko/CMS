7b0dbf63d37f104fa4101c7e47cc66a8f72e5c50
Thenilko <netgames@abv.bg>
2022-02-09 17:16:41 +0200

    Auto commit and push

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #git config --global user.email "netgames@abv.bg"
 #git config --global user.name "Thenilko"
 
-git remote set-url origin https://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@github.com/Thenilko/CMS.git
+git remote set-url origin https://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@github.com/Thenilko/CMS.git
 git status
 git add .
 git commit -m 'Auto commit and push'
