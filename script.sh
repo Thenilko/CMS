8d7ad1d1be7f314eb8417b2a2cc1c231ca7714dd
Thenilko <netgames@abv.bg>
2022-02-09 16:58:35 +0200

    Auto commit and push

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 #git config --global user.email "netgames@abv.bg"
 #git config --global user.name "Thenilko"
 
-#curl -u "Thenilko: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" https://github.com/Thenilko/CMS.git
+curl -u "Thenilko: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" https://github.com/Thenilko/CMS.git
 git status
 git add .
 git commit -m 'Auto commit and push'
