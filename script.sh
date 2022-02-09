eac19e59934ded5216e9b3be6bdbb2334dbf9da8
Thenilko <netgames@abv.bg>
2022-02-09 16:28:13 +0200

    Auto commit and push

--- script.sh
+++ script.sh
@@ -3,7 +3,10 @@
 #git config --global user.email "netgames@abv.bg"
 #git config --global user.name "Thenilko"
 
-curl -u "Thenilko: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" https://github.com/Thenilko/CMS.git
+#curl -u "Thenilko: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" https://github.com/Thenilko/CMS.git
 git add .
 git commit -m 'Auto commit and push'
 git push origin main
+
+expect "Username for 'https://github.com':"
+send "Thenilko"
